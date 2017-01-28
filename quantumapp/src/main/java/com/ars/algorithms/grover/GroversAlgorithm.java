1a7b8c45e730c9ac13cbfe32e271bde7587445e5
ardeleanasm <ardeleanasm@gmail.com>
2017-01-28 19:07:35 +0200

    Fix apps #17

--- quantumapp/src/main/java/com/ars/algorithms/grover/GroversAlgorithm.java
+++ quantumapp/src/main/java/com/ars/algorithms/grover/GroversAlgorithm.java
@@ -1,118 +1,71 @@
 package com.ars.algorithms.grover;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.function.Function;
 
 import com.ars.algorithms.MeasurementPerformer;
 import com.ars.algorithms.QuantumAlgorithms;
-import com.ars.complexnumbers.ComplexNumber;
 import com.ars.gates.EGateTypes;
 import com.ars.gates.IGate;
-import com.ars.quantum.utils.MatrixOperations;
+import com.ars.quantum.exception.RegisterOverflowException;
+import com.ars.quantum.utils.QRegisterOperations;
 import com.ars.quantum.utils.QuantumOperations;
-import com.ars.qubits.Qubit;
-import com.ars.qubits.QubitPlus;
-import com.ars.qubits.QubitZero;
+import com.ars.qubits.QRegister;
 
 public class GroversAlgorithm extends QuantumAlgorithms {
 	private static final int NO_OF_INPUT = 3;
 	private IGate gateH;
-	private double[][] gateHn;
-	private static final Qubit QUBIT_0 = new QubitZero();
-	private Qubit qubitPlus;
+	private IGate gateX;
+	private IGate gateCNot;
+	private IGate gateCPhaseShift;
+	
 
-	private double[][] oracleMatrix = { { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
-			{ 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
-			{ 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 },
-			{ 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 },
-			{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0 } };
-	private BufferedWriter bw;
-	private ComplexNumber[][] diffusionMatrix;
+	public GroversAlgorithm() {
 
-	public GroversAlgorithm(String filePath) throws IOException, SecurityException {
-		bw = new BufferedWriter(new FileWriter(new File(filePath)));
+
 	}
 
-	public GroversAlgorithm() throws IOException, SecurityException {
-		File outputDir = new File("output");
-
-		if (!outputDir.exists())
-			outputDir.mkdir();
-
-		bw = new BufferedWriter(new FileWriter(new File(outputDir, "out.csv")));
-	}
-
-	private void generateDiffusionMatrix() {
-		diffusionMatrix = QuantumOperations.outerProduct(qubitPlus, qubitPlus);
-		diffusionMatrix = MatrixOperations.multiplyByConstant(diffusionMatrix, 2);
-		ComplexNumber[][] identityMatrix = MatrixOperations.generateIdentityMatrix(8);
-		diffusionMatrix = MatrixOperations.subtract(diffusionMatrix, identityMatrix);
-	}
 
 	@Override
 	public void init() {
 		gateH = gateFactory.getGate(EGateTypes.E_HadamardGate);
-		resultQubit = QUBIT_0;
-		qubitPlus = new QubitPlus();
-		for (int i = 0; i < NO_OF_INPUT - 1; i++) {
-			resultQubit = QuantumOperations.entangle(resultQubit, QUBIT_0);
+		gateX = gateFactory.getGate(EGateTypes.E_XGate);
+		gateCNot = gateFactory.getGate(EGateTypes.E_CNotGate);
+		gateCPhaseShift = gateFactory.getGate(EGateTypes.E_CPhaseShift);
+		QRegister qRegister=new QRegister(NO_OF_INPUT+1);
+		QRegisterOperations qRegOps=QRegisterOperations.getInstance();
+		try {
+			qRegister=qRegOps.fillWithPattern("0001");
+		} catch (RegisterOverflowException e) {
+			e.printStackTrace();
 		}
-		for (int i = 0; i < NO_OF_INPUT - 1; i++) {
-			qubitPlus = QuantumOperations.entangle(qubitPlus, new QubitPlus());
-		}
-		gateHn = gateH.getUnitaryMatrix();
-		for (int i = 0; i < NO_OF_INPUT - 1; i++) {
-			gateHn = MatrixOperations.tensorProduct(gateHn, gateH.getUnitaryMatrix());
-		}
-		setOracle(oracleMatrix);
-		generateDiffusionMatrix();
+		resultQubit=QuantumOperations.entangle(qRegister);
 	}
 
 	@Override
 	public void run() {
-		int noOfIterations = (int) Math.sqrt(Math.pow(2, NO_OF_INPUT));
-		resultQubit = QuantumOperations.applyGate(resultQubit, gateHn);
-		for (int i = 0; i < noOfIterations + 1; i++) {
-			resultQubit = QuantumOperations.applyGate(resultQubit, oracleMatrix);
-			resultQubit = QuantumOperations.applyGate(resultQubit, diffusionMatrix);
-		}
-		assert (resultQubit.isValid() == true);
+		resultQubit=gateH.applyGate(resultQubit, new int[]{0,1,2,3}, null, NO_OF_INPUT+1);
+		resultQubit=gateCNot.applyGate(resultQubit, new int[]{3}, new int[]{0,1,2}, NO_OF_INPUT+1);
+		
+		
+		resultQubit=gateH.applyGate(resultQubit, new int[]{0,1,2}, null, NO_OF_INPUT+1);
+		resultQubit=gateX.applyGate(resultQubit, new int[]{0,1,2}, null, NO_OF_INPUT+1);
+		resultQubit=gateCPhaseShift.applyGate(resultQubit, new int[]{2}, new int[]{0,1}, NO_OF_INPUT+1);
+		
+		resultQubit=gateX.applyGate(resultQubit, new int[]{0,1,2}, null, NO_OF_INPUT+1);
+		resultQubit=gateH.applyGate(resultQubit, new int[]{0,1,2,3}, null, NO_OF_INPUT+1);
+		
+		
+		
+		
 	}
 
 	@Override
 	public void measure() {
 		MeasurementPerformer measurementPerformer = new MeasurementPerformer().configure(resultQubit);
 		resultQubit = measurementPerformer.measure();
-
-		try {
-
-			for (ComplexNumber c : resultQubit.getQubit()) {
-				bw.write(c.getReal() + ",");
-
-			}
-			bw.write("\n");
-			// bw.newLine();
-			// bw.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println(resultQubit);
 
 	}
 
-	public void close() {
-		try {
-			bw.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 
 }
--- quantumapp/src/main/java/com/ars/quantumapp/GroverRunner.java
+++ quantumapp/src/main/java/com/ars/quantumapp/GroverRunner.java
@@ -1,28 +1,18 @@
 package com.ars.quantumapp;
 
-import java.io.IOException;
 
 import com.ars.algorithms.grover.GroversAlgorithm;
 
-public class GroverRunner implements Runnable{
+public class GroverRunner implements Runnable {
 
 	@Override
 	public void run() {
 		GroversAlgorithm ga = null;
-		try{
-			ga = new GroversAlgorithm();
-			for (int i = 0; i < 1000000; i++) {
-				ga.init();
-				ga.run();
-				ga.measure();
-			}
-		} catch (SecurityException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if (ga != null)
-				ga.close();
-		}
+		ga = new GroversAlgorithm();
+		ga.init();
+		ga.run();
+		ga.measure();
+		
 	}
 
 }
